24862a4991908dcc8e726635304adc90da5cb6c8
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-14 16:56:19 +0000

    cw-two - initial implementation of IAdd ByteCode

--- src/main/scala/bc/IAdd.scala
+++ src/main/scala/bc/IAdd.scala
@@ -1,8 +1,21 @@
 package bc
+import vm.VirtualMachine
 
 /**
   * @author lmignot
   */
-class IAdd {
+case class IAdd() extends ByteCode {
+  /**
+    * @see [[bc.ByteCode.code]]
+    */
+  override val code: Byte = bytecode("iadd")
 
+  /**
+    * @see [[bc.ByteCode.execute()]]
+    */
+  override def execute(vm: VirtualMachine): VirtualMachine = {
+    val (x, vm2) = vm.pop()
+    val (y, _) = vm2.pop()
+    vm2.push(x + y)
+  }
 }
