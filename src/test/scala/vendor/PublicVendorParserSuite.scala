774915b6f99fe7391a844394fdd4cfcb0c614f58
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-14 15:53:35 +0000

    cw-two - some edge-case tests for invalid instructions

--- src/test/scala/vendor/PublicVendorParserSuite.scala
+++ src/test/scala/vendor/PublicVendorParserSuite.scala
@@ -6,6 +6,24 @@ import org.scalatest.FunSuite
 class PublicVendorParserSuite extends FunSuite {
   val vp = VirtualMachineFactory.vendorParser
 
+  test("[0] vendor parser throw if parsing unrecognised instruction") {
+    assertThrows[vendor.InvalidInstructionFormatException] {
+      val insts = vp.parseString("ssss\n")
+    }
+  }
+
+  test("[1] vendor parser throw if parsing bad program") {
+    assertThrows[vendor.InvalidInstructionFormatException] {
+      val insts = vp.parseString("programs/p04-bad-program.vm")
+    }
+  }
+
+  test("[2] vendor parser throw if parsing invalid arguments") {
+    assertThrows[vendor.InvalidInstructionFormatException] {
+      val insts = vp.parseString("iconst x\niconst aa\niadd\nprint")
+    }
+  }
+
   test("[3] vendor parser should parse a program string correctly") {
     val insts = vp.parseString("iconst 4\niconst 5\niadd\nprint")
     assert(insts.length == 4)
