bd49286482e04d5c327920d6cbc03dee2a2620f4
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-16 15:05:26 +0000

    cw-two - implement ByteCodeFactory

--- src/main/scala/bc/ByteCodeFactoryImpl.scala
+++ src/main/scala/bc/ByteCodeFactoryImpl.scala
@@ -1,8 +1,36 @@
 package bc
+import bc.ByteCodes._
 
 /**
-  * @author lmignot
+  * @see [[ByteCodeFactory]]
   */
-class ByteCodeFactoryImpl {
+object ByteCodeFactoryImpl extends ByteCodeFactory with ByteCodeValues {
 
+  /**
+    * [[ByteCodeValues.bytecode]] maps a String representing a ByteCode
+    * instruction to some Byte value. We require a reverse of this map so we can
+    * match a Byte value to its String representation
+    */
+  private lazy val byteToNames: Map[Byte, String] = bytecode.map(_.swap)
+
+  /**
+    * @see [[ByteCodeFactory.make]]
+    */
+  override def make(byte: Byte, args: Int*): ByteCode = byteToNames.get(byte) match {
+    case Some("iadd") => IAdd()
+    case Some("isub") => ISub()
+    case Some("imul") => IMul()
+    case Some("idiv") => IDiv()
+    case Some("irem") => IRem()
+    case Some("ineg") => INeg()
+    case Some("iinc") => IInc()
+    case Some("idec") => IDec()
+    case Some("iswap") => ISwap()
+    case Some("idup") => IDup()
+    case Some("print") => Print()
+    case Some("iconst") if args.isEmpty =>
+      throw new InvalidBytecodeException("IConst requires one argument")
+    case Some("iconst") => IConst(args.head)
+    case _ => throw new InvalidBytecodeException(s"$byte is an invalid ByteCode")
+  }
 }
