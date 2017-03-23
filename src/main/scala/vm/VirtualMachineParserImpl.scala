28004bb836e473c521d6e9441c92514a09e96a5a
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-23 16:47:15 +0000

    cw-two - implement VirtualMachineParser

--- src/main/scala/vm/VirtualMachineParserImpl.scala
+++ src/main/scala/vm/VirtualMachineParserImpl.scala
@@ -1,18 +1,61 @@
 package vm
-import bc.ByteCode
+import bc.{ByteCode, ByteCodeParser, InvalidBytecodeException}
+import vendor.{Instruction, InvalidInstructionFormatException, ProgramParser}
 
 /**
   * @see [[VirtualMachineParser]]
   */
-object VirtualMachineParserImpl extends VirtualMachineParser {
+case class VirtualMachineParserImpl(vParser: ProgramParser,
+                                    bcParser: ByteCodeParser)
+  extends VirtualMachineParser {
+
+  type InstructionList = Vector[Instruction]
+  type ByteCodeList = Vector[ByteCode]
 
   /**
     * @see [[VirtualMachineParser.parse()]]
     */
-  override def parse(file: String): Vector[ByteCode] = ???
+  override def parse(file: String): ByteCodeList =
+    adapt(tryToParse(file, vParser.parse))
 
   /**
     * @see [[VirtualMachineParser.parseString()]]
     */
-  override def parseString(str: String): Vector[ByteCode] = ???
+  override def parseString(str: String): ByteCodeList =
+    adapt(tryToParse(str, vParser.parseString))
+
+  /**
+    * Catches [[InvalidInstructionFormatException]]
+    * and throws the expected [[InvalidBytecodeException]]
+    * instead
+    *
+    * @param input String containing codes to be parsed
+    * @param f Function that takes a String and returns
+    *          an [[InstructionList]]
+    * @return The parsed [[InstructionList]] if successful
+    */
+  private def tryToParse(input: String,
+                  f: String => InstructionList): InstructionList = {
+    try f(input)
+    catch {
+      case ex: InvalidInstructionFormatException =>
+        throw new InvalidBytecodeException(ex.getMessage)
+    }
+  }
+
+  /**
+    * Adapts an [[InstructionList]] to a [[ByteCodeList]]
+    *
+    * @param input [[InstructionList]] to adapt
+    * @return [[ByteCodeList]] with each instruction adapted
+    *         as a [[ByteCode]]
+    */
+  private def adapt(input: InstructionList): ByteCodeList = {
+    var output: Vector[Byte] = Vector.empty
+    for (el <- input) {
+      output :+= bcParser.bytecode(el.name)
+      if (el.args.nonEmpty) output = output ++ el.args.map(_.toByte)
+    }
+    bcParser.parse(output)
+  }
 }
--- src/test/scala/vm/PublicVirtualMachineParserSuite.scala
+++ src/test/scala/vm/PublicVirtualMachineParserSuite.scala
@@ -7,7 +7,7 @@ import org.scalatest.FunSuite
 class PublicVirtualMachineParserSuite extends FunSuite with ByteCodeValues {
   val vmp = VirtualMachineFactory.virtualMachineParser
 
-  test("[8] vm parser should parse programs/p01.vm a file into bytecode") {
+  test("[13] vm parser should parse programs/p01.vm a file into bytecode") {
     val code = vmp.parse("programs/p01.vm")
     assert(code.length == 4)
     assert(code(0).code == bytecode("iconst"))
@@ -16,7 +16,7 @@ class PublicVirtualMachineParserSuite extends FunSuite with ByteCodeValues {
     assert(code(3).code == bytecode("print"))
   }
 
-  test("[8] vm parser should parse programs/p03.vm a file into bytecode") {
+  test("[14] vm parser should parse programs/p03.vm a file into bytecode") {
     val code = vmp.parse("programs/p03.vm")
     assert(code.length == 20)
     val all = Vector("iconst", "iconst", "iswap", "iadd", "iconst", "iadd",
@@ -27,7 +27,7 @@ class PublicVirtualMachineParserSuite extends FunSuite with ByteCodeValues {
     }
   }
 
-  test("[4] vm parser should detect a program with invalid bytecode") {
+  test("[15] vm parser should detect a program with invalid bytecode") {
     intercept[InvalidBytecodeException] {
       val code = vmp.parse("programs/p04-bad-program.vm")
     }
