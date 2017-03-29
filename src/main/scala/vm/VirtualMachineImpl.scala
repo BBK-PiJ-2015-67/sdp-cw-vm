f3f5f011f11d0a89afbb61f9959fe568a3dcee19
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-29 14:07:06 +0100

    cw-two - implementation of VirtualMachine

--- src/main/scala/vm/VirtualMachineImpl.scala
+++ src/main/scala/vm/VirtualMachineImpl.scala
@@ -2,31 +2,43 @@ package vm
 import bc.ByteCode
 
 /**
+  * [[VirtualMachine]] implementation
+  * Implemented as a case class as it's reasonable
+  * to assume that multiple virtual machine's could be
+  * required
+  *
   * @see [[VirtualMachine]]
   */
-case class VirtualMachineImpl() extends VirtualMachine {
+case class VirtualMachineImpl(private val stack: Vector[Int] = Vector.empty) extends VirtualMachine {
+
   /**
     * @see [[VirtualMachine.execute()]]
     */
-  override def execute(bc: Vector[ByteCode]): VirtualMachine = ???
+  override def execute(bc: Vector[ByteCode]): VirtualMachine =
+    bc.foldLeft(this.asInstanceOf[VirtualMachine])((vm, code) => code.execute(vm))
 
   /**
     * @see [[VirtualMachine.executeOne()]]
     */
-  override def executeOne(bc: Vector[ByteCode]): (Vector[ByteCode], VirtualMachine) = ???
+  override def executeOne(bc: Vector[ByteCode]): (Vector[ByteCode], VirtualMachine) =
+    (bc.tail, bc.head.execute(this))
 
   /**
     * @see [[VirtualMachine.push()]]
     */
-  override def push(value: Int): VirtualMachine = ???
+  override def push(value: Int): VirtualMachine = VirtualMachineImpl(value +: stack)
 
   /**
     * @see [[VirtualMachine.pop()]]
+    * @throws MachineUnderflowException if the VM's state is empty
     */
-  override def pop(): (Int, VirtualMachine) = ???
+  override def pop(): (Int, VirtualMachine) = {
+    if (stack.isEmpty) throw new MachineUnderflowException("Virtual machine has an empty state")
+    (stack.head, VirtualMachineImpl(stack.tail))
+  }
 
   /**
     * @see [[VirtualMachine.state]]
     */
-  override def state: Vector[Int] = ???
+  override def state: Vector[Int] = stack
 }
