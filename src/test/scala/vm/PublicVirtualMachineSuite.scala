901d75418b5775a7b764cf11fc2c885e5772c6f9
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-29 14:07:28 +0100

    cw-two - more pvm tests

--- src/test/scala/vm/PublicVirtualMachineSuite.scala
+++ src/test/scala/vm/PublicVirtualMachineSuite.scala
@@ -4,22 +4,47 @@ import factory.VirtualMachineFactory
 import org.scalatest.FunSuite
 
 class PublicVirtualMachineSuite extends FunSuite {
+  val TestInt = 99
+
   val vmp = VirtualMachineFactory.virtualMachineParser
   val bcp = VirtualMachineFactory.byteCodeParser
   val vm  = VirtualMachineFactory.virtualMachine
 
-  test("[10] a virtual machine should execute a program") {
+  test("[16] vm.push should work") {
+    val vm2 = vm.push(TestInt)
+    assert(vm2.state.head == TestInt)
+    assert(vm2 ne vm)
+  }
+
+  test("[17] vm.pop should work") {
+    val vm2 = vm.push(TestInt)
+    val (top, vm3) = vm2.pop()
+    assert(vm2.state.head == TestInt)
+    assert(vm3.state.isEmpty)
+    assert(top == TestInt)
+    assert(vm2 ne vm)
+    assert(vm3 ne vm2)
+    assert(vm3 ne vm)
+  }
+
+  test("[18] vm.pop should throw if empty") {
+    intercept[MachineUnderflowException] {
+      vm.pop()
+    }
+  }
+
+  test("[19] a virtual machine should execute a program") {
     val bc  = vmp.parse("programs/p05.vm")
     val vm2 = vm.execute(bc)
   }
 
-  test("[2] iconst should work correctly") {
+  test("[20] iconst should work correctly") {
     val bc  = vmp.parseString("iconst 1")
     val (bc2, vm2) = vm.executeOne(bc)
     assert(vm2.state.head == 1)
   }
 
-  test("[2] iadd should work correctly") {
+  test("[21] iadd should work correctly") {
     val bc  = vmp.parseString("iconst 1\niconst 2\niadd")
     var next = vm.executeOne(bc)
     assert(next._2.state.head == 1)
@@ -29,7 +54,7 @@ class PublicVirtualMachineSuite extends FunSuite {
     assert(next._2.state.head == 3)
   }
 
-  test("[2] isub should work correctly") {
+  test("[22] isub should work correctly") {
     val bc  = vmp.parseString("iconst 1\niconst 2\nisub")
     var next = vm.executeOne(bc)
     assert(next._2.state.head == 1)
@@ -39,7 +64,7 @@ class PublicVirtualMachineSuite extends FunSuite {
     assert(next._2.state.head == 1)
   }
 
-  test("[2] iswap should work correctly") {
+  test("[23] iswap should work correctly") {
     val bc  = vmp.parseString("iconst 1\niconst 2\niswap")
     var next = vm.executeOne(bc)
     assert(next._2.state.head == 1)
